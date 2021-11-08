c0514e1a55d1ec76118061e90d688eec01e6eccc
Vay Kaushik <vay.kaushik@gmail.com>
2021-11-08 11:27:10 +0000

    updated images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Wealth Generator with Random User API',
     description: "Using React, Sass and the Axios library, I retrieved the first and last names from the API and was able to display this with a random wealth amount too based on what is clicked.",
-      image: '/images/1.gif',
+      image: '/images/1.png',
       tags: ['React', 'Sass', 'Axios'],
     source: 'https://github.com/vaykaushik/user-and-dog-api',
     visit: 'https://react-api-practice.netlify.app/',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Dictionary App with Dictionary API',
     description:"This dictionary app used the dictionary API to retrieve the user's word definition, pronunciation and phonetics. The project taught me how to use vanilla JS to make API calls using the Fetch method.",
-    image: '/images/2.gif',
+    image: '/images/2.png',
     tags: ['JavaScript', 'Sass'],
     source: 'https://github.com/vaykaushik/dictionary-app-final',
     visit: 'https://dictionary-app-final.netlify.app/',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Weather App with API',
     description: "Using React, Sass and the Axios library together with Async/Await, this allows the user to search for any location and retrieves information regarding temperature, humidity and much more.",
-      image: '/images/3.gif',
+      image: '/images/3.png',
       tags: ['React', 'Sass', 'Axios', 'Async/Await'],
     source: 'https://github.com/vaykaushik/react-weather-app-version',
     visit: 'https://react-weather-app-version.netlify.app/',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Clipboard Landing Page',
     description: "Using React and Sass, I converted a .png design for Desktop and Mobile from Front-End Mentor into code that is fully response and ready for all different screen sizes.",
-    image: '/images/4.gif',
+    image: '/images/4.png',
     tags: ['React', 'Sass'],
     source: 'https://github.com/vaykaushik/clipboard-react-version-final',
     visit: 'https://clipboard-react-version.netlify.app/',
